ff9e3dc0558d778539296de40500e04abc2f3586
Parthahuja03 <parthahuja2903@gmail.com>
2026-02-01 03:32:36 +0530

    end of first pratice class

--- start.py
+++ start.py
@@ -1,2 +1,18 @@
 print("hi")
 print("First code")
+
+# concatination
+print("Hello parth", 1 , "Hi")
+
+# add a seperator
+print("Hello parth", 1 , "Hi", sep='*')
+
+#---triple quotes
+print('''This is our practice codes
+of python''')
+
+#--- back slash
+print('''This is\' our practice codes
+of python''')
+
+
