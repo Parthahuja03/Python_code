5b09e8eaf8cc8586ef6a9254dcbd1682f7a1346f
Parthahuja03 <parthahuja2903@gmail.com>
2026-02-01 03:51:17 +0530

    Python variables and type casting practice

---
+++ python_variables.py
@@ -0,0 +1,47 @@
+my_name ="My name is parth"
+print(my_name)
+
+my_age=23
+print(my_name,"My age is ",my_age)
+
+
+x=10 
+y=5
+print(x+y)
+
+#concatination of Strings
+first_name ="parth"
+last_name='Ahuja'
+print(first_name + last_name)
+
+#String + int doesnt work
+'''age =23
+last_name='Ahuja'
+print(age + last_name)'''
+
+#multiple Assignments
+a,b,c =10,5,20
+
+
+#multiple line codes
+totalSum = 10+20+30\
+            +40+50
+total_sum=(10+20
+           +30)
+print(totalSum," ", total_sum)
+
+
+#type casting
+
+aa=10
+bb="10"
+print(type(bb))
+
+#explicit
+b_new = int(bb)
+print(b_new)
+
+#implicit
+cc=10.5
+print(aa+cc)
+
