ae72d6ed0720f0308cf85e7e1d204e85e25e4709
Parthahuja03 <parthahuja2903@gmail.com>
2026-02-01 04:06:26 +0530

    Practicing strings in python

---
+++ string_formatting.py
@@ -0,0 +1,16 @@
+#String act as an array
+name ="parth"
+print(name[0])
+
+#slicing
+print(name[0:3])  # 3rd index is not included
+print(name[:])  #auto from 0 to n (length)
+print(name[0:len(name)])
+
+
+#String Methods
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+
+#Explore by your own now
