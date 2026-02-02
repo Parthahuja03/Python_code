cc3515496639f7eeb79198bfb49b8b6dec70ccca
Parthahuja03 <parthahuja2903@gmail.com>
2026-02-02 19:37:36 +0530

    Conditional Statements in Python

---
+++ conditionalsStatement.py
@@ -0,0 +1,16 @@
+x =22
+
+if(x==10):
+    print("the number is 10")
+
+
+#else if condition  
+elif(x>10):
+    print("X is greater than 10")
+    #Nested loops
+    if((x>20)&(x<25)): #use braces in conditions
+        print("x is between 20 and 25")
+
+#else condition
+else: 
+    print("The number is not 10")
