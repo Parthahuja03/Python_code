194ba5539574a9af4af2da35f5e296765ba32ffb
Parthahuja03 <parthahuja2903@gmail.com>
2026-02-08 03:09:17 +0530

    Function completed

--- functions.py
+++ functions.py
@@ -47,6 +47,69 @@ print(add(2,3))
 
 #MAP_FILTER_REDUCE
 
+myList =[1,2,3,4,5]
+
+def square(p_x):
+    ret_list =[i*i for i in p_x]
+    print(ret_list)
+    
+square(myList)
+
+#Map
+myList =[1,2,3,4,5]
+def squareInd(p_x):
+    return p_x*p_x
+
+result = list(map(squareInd,myList))#- map each item of myList and passing it in square function
+print(result)
+
+#Filter
+myList =[1,2,3,4,5]
+def square(p_x):
+    if(p_x%2==0):
+        return p_x*p_x #- in that case , none will be printed as it is not filtering out the odd cases
+
+result = list(map(square,myList))#- in that case , none will be printed as it is not filtering out the odd cases
+
+result2 = list(filter(square,myList))
+print(result)
+print(result2)
+
+#Reduce
+
+from functools import reduce # This is not default imported , we need to import it in every piece of code
+
+myList =[1,2,3,4,5]
+def squareInd(p_x,p_y):
+    return p_x*p_y
+result = reduce(squareInd,myList)
+print(result)
+
+
+#fString
+name="Parth"
+# Just by putting f before the "", it will take the variable into the string
+print(f"my name is {name}")
+
+
+#Custom error and exception 
+
+x=99
+
+if(x<100):
+    raise ValueError("value< 100 is not allowed")
+
+if(x<100):
+    raise Exception("value< 100 is not allowed")
+
+
+#enumerate
+
+myList =[10,20,30,40]
+for i in enumerate (myList):
+    print(i)
+for i,x in enumerate(myList):
+    print(x)
 
 
 
